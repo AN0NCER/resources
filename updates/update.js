a8d0fe916ca3cef72785f3c5657c47a8f5d9f73b
Anoncer <anoncer@protonmail.com>
2024-11-04 15:00:51 +0100

    Update Resource

---
+++ updates/update.js
@@ -0,0 +1,52 @@
+const v256 = (callback = () => {}) => {
+    const loadDbAnime = (key) => {
+        return JSON.parse(localStorage.getItem(key)) || {};
+    };
+
+    const saveDbAnime = (key, value) => {
+        localStorage.setItem(key, JSON.stringify(value));
+    }
+
+    const typeOf = (object) => {
+        return Object.keys(object).toString();
+    };
+
+    const lKey = "anime-db"
+    const synch = $PARAMETERS.anime.syncdata;
+    const keys = Object.keys(localStorage);
+    const dbAnime = loadDbAnime(lKey);
+
+    const processUpdate = (value, key) => {
+        const ldata = value;
+        if (!dbAnime[key]) {
+            dbAnime[key] = { synch, ldata };
+        }
+        localStorage.removeItem(key);
+    };
+
+    const passKey = ["anime-db", "access_token", "access_whoami", "anime-collection", "application_installed", "dialog-update", "github-version", "last-watch", "parametrs", "tunime-id"];
+    const inclType = ['kodik_episode,kodik_dub,date_update', 'kodik_episode,kodik_dub'];
+
+    keys.forEach(key => {
+        if (passKey.includes(key))
+            return;
+
+        const value = loadDbAnime(key);
+
+        if (!inclType.includes(typeOf(value)))
+            return;
+
+        processUpdate(value, key);
+    });
+
+    saveDbAnime(lKey, dbAnime);
+    console.log(`[update] - Update data to v2.5.6`);
+    callback();
+}
+
+
+export function initialize(version, callback = () => {}) {
+    if (version === "2.5.6") {
+        v256(callback);
+    }
+}
