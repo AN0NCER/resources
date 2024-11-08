28e9bb046495642e3074115886e0f86041920d69
Anoncer <anoncer@protonmail.com>
2024-11-08 19:01:04 +0100

    Setup Update Tunime to v2.6.0

--- t-update/v_2.6.0.js
+++ t-update/v_2.6.0.js
@@ -1,49 +1,51 @@
 export default () => {
-    try {
-        const loadDbAnime = (key) => {
-            return JSON.parse(localStorage.getItem(key)) || {};
-        };
+    return new Promise((resolve) => {
+        try {
+            const loadDbAnime = (key) => {
+                return JSON.parse(localStorage.getItem(key)) || {};
+            };
 
-        const saveDbAnime = (key, value) => {
-            localStorage.setItem(key, JSON.stringify(value));
-        }
-
-        const typeOf = (object) => {
-            return Object.keys(object).toString();
-        };
-
-        const lKey = "anime-db"
-        const synch = $PARAMETERS.anime.syncdata;
-        const keys = Object.keys(localStorage);
-        const dbAnime = loadDbAnime(lKey);
-
-        const processUpdate = (value, key) => {
-            const ldata = value;
-            if (!dbAnime[key]) {
-                dbAnime[key] = { synch, ldata };
+            const saveDbAnime = (key, value) => {
+                localStorage.setItem(key, JSON.stringify(value));
             }
-            localStorage.removeItem(key);
-        };
 
-        const passKey = ["anime-db", "access_token", "access_whoami", "anime-collection", "application_installed", "dialog-update", "github-version", "last-watch", "parametrs", "tunime-id"];
-        const inclType = ['kodik_episode,kodik_dub,date_update', 'kodik_episode,kodik_dub'];
+            const typeOf = (object) => {
+                return Object.keys(object).toString();
+            };
 
-        keys.forEach(key => {
-            if (passKey.includes(key))
-                return;
+            const lKey = "anime-db"
+            const synch = $PARAMETERS.anime.syncdata;
+            const keys = Object.keys(localStorage);
+            const dbAnime = loadDbAnime(lKey);
 
-            const value = loadDbAnime(key);
+            const processUpdate = (value, key) => {
+                const ldata = value;
+                if (!dbAnime[key]) {
+                    dbAnime[key] = { synch, ldata };
+                }
+                localStorage.removeItem(key);
+            };
 
-            if (!inclType.includes(typeOf(value)))
-                return;
+            const passKey = ["anime-db", "access_token", "access_whoami", "anime-collection", "application_installed", "dialog-update", "github-version", "last-watch", "parametrs", "tunime-id"];
+            const inclType = ['kodik_episode,kodik_dub,date_update', 'kodik_episode,kodik_dub'];
 
-            processUpdate(value, key);
-        });
+            keys.forEach(key => {
+                if (passKey.includes(key))
+                    return;
 
-        saveDbAnime(lKey, dbAnime);
-        console.log(`[update] - Update data to ${lKey}`);
-        return "success";
-    } catch (error) {
-        return "error";
-    }
+                const value = loadDbAnime(key);
+
+                if (!inclType.includes(typeOf(value)))
+                    return;
+
+                processUpdate(value, key);
+            });
+
+            saveDbAnime(lKey, dbAnime);
+            console.log(`[update] - Update data to ${lKey}`);
+            resolve("success");
+        } catch (error) {
+            resolve("error");
+        }
+    });
 };
